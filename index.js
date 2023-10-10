22c9e7eba3ee674dd54f8656de4a0d6a53f77b94
mjpfelicia <86963473+mjpfelicia@users.noreply.github.com>
2023-10-10 23:56:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+
+alturas = []
+
+// Implementar a entrada de dados
+
+// Implementar o processamento de dados
+// Fazer for dentro de for
+
+// Implementar a saída de dados
+
