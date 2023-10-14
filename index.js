54b41b832078e0ae61b04dac11e7e48eac8a5842
Sandra Felicio <mjpfelicia@gmail.com>
2023-10-14 17:03:42 -0300

    execultando exercico javascript array

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const prompt = require('prompt-sync')();
 
-alturas = []
+var alturas = []
 
 // Implementar a entrada de dados
 
@@ -8,4 +8,19 @@ alturas = []
 // Fazer for dentro de for
 
 // Implementar a saída de dados
+
+
+
+
+for (let altura = 1;  altura <= 10; altura++) {
+    alturas.push(parseFloat(prompt("Digite sua altura:")))
+
+}
+console.log(alturas)
+for (let index = 0; index< alturas.length; index++) {
+    const aluno = alturas[index];
+
+   const quantidadeDeAlturasMenores = alturas.filter(altura => altura < aluno ).length
+ console.log(`aluno ${index} : maior que ${quantidadeDeAlturasMenores} aluno(s)`)   
+}
 
